f871ee6d7c9cc097dc0c2d0061bda4a5ab265de8
Name <anushqakandan03@gmail.com>
2024-08-29 11:56:58 +0200

    store

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,85 @@
 <template>
   <div class="container">
-    <div class="row">
-      <h2 class="display-2">Luminous </h2>
+    <div class="backdrop">
+ <div class="row">
+      <h2 class="display-2">Luminous Gems</h2>
     </div>
     <div class="row">
       <h4 class="display-4">Sparkle Your World, One Gem at a Time</h4>
     </div>
-    <div class="row gap-2 justify-content-center" v-if="recentProducts">
+    </div>
+   
+  
+  
+  <section class="service">
+    <div class="row mt-5 meh">
+      <div class="col-3">
+        <div class="row">
+          <div class="col-1"></div>
+          <div class="col-3">
+            <i class="las la-truck"></i>
+          </div>
+          <div class="col-5">
+            <h5><b>Free shipping</b></h5>
+            <p>Free Shipping On All Orders</p>
+          </div>
+          <div class="col-1"></div>
+        </div>
+      </div>
+      <div class="col-3">
+        <div class="row">
+          <div class="col-1"></div>
+          <div class="col-3">
+            <i class="las la-check-circle"></i>
+          </div>
+          <div class="col-5">
+            <h5><b>Money Guarantee</b></h5>
+            <p>30 Day Money Back</p>
+          </div>
+          <div class="col-1"></div>
+        </div>
+        
+      </div>
+      <div class="col-3">
+        <div class="row">
+          <div class="col-1"></div>
+          <div class="col-3">
+            <i class="las la-headphones"></i>
+          </div>
+          <div class="col-5">
+            <h5><b>Online Support 24/7</b></h5>
+            <p>Technical Support 24/7</p>
+          </div>
+          <div class="col-1"></div>
+        </div>
+      </div>
+      <div class="col-3">
+        <div class="row">
+          <div class="col-1"></div>
+          <div class="col-3">
+            <i class="las la-wallet"></i>
+          </div>
+          <div class="col-5">
+            <h5><b>Secure Payment</b></h5>
+            <p>AI Cash Accepted</p>
+          </div>
+          <div class="col-1"></div>
+        </div>
+       
+      </div>
+    </div>
+  </section>
 
-      <Card v-for="product in recentProducts" :key="product.productID">
+
+
+    <h2>Recent Products</h2>
+    <div class="row gap-2 justify-content-center" v-if="recentProducts">
+      <Card v-for="product in recentProducts" :key="product.prodID">
         <template #cardHeader>
-          {{ product.productURL }}
           <img :src="product.prodURL" loading="lazy" class="img-fluid" :alt="product.prodName">
         </template>
         <template #cardBody>
           <h5 class="card-title fw-bold">{{ product.prodName }}</h5>
-          <p class="lead">{{ product.prodDescription }}</p>
           <p class="lead"><span class="text-success fw-bold">Amount</span>: R{{ product.amount }}</p>
         </template>
       </Card>
@@ -45,3 +109,28 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.backdrop { 
+background-image: url('https://anushqakandan.github.io/Capstone_images/Main3.webp');
+    background-size: cover; 
+    background-repeat: no-repeat; 
+    height: 85vh;
+}
+
+@media only screen and (max-width: 767px) {
+    .backdrop {
+        background-position: center; /* Adjust to display a different part for mobile view */
+    }
+}
+
+.display-2 {
+color: white;
+}
+
+.display-4{
+  color: goldenrod;
+  margin-top: 28rem;
+}
+
+</style>
